5b0d603a551b3d1c586cddf3b50a683cf4f398a2
Alush111 <enanushe@gmail.com>
2022-06-06 09:31:05 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * main - This only prints the last digit of n
+ *
+ * Return: 0 if all is ok
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 122; i > 96; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
