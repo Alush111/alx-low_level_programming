5ab67f21ed714145b9ec631fe4478853c4016780
Alush111 <enanushe@gmail.com>
2022-06-08 08:12:18 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Print alphabet lowercase
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		_putchar(alphabet);
+	}
+
+	_putchar('\n');
+
+}
